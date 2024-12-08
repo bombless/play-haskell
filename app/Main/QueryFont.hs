2aef854eb84979f70b30e8b942ad660392d70292
York Xiang <bombless@126.com>
2024-12-08 19:39:13 +0800

    Open any file to query font!

--- app/Main/QueryFont.hs
+++ app/Main/QueryFont.hs
@@ -1,11 +1,12 @@
 module Main.QueryFont (queryFont) where
 
 
-import Control.Monad (filterM)
+-- import Control.Monad (filterM)
 import Text.Printf (printf)
 
 import System.Directory (listDirectory)
-import System.FilePath ((</>), takeExtension)
+import System.FilePath ((</>))
+-- import System.FilePath (takeExtension)
 
 import Foreign.C.String (withCString)
 import Foreign.Ptr (nullPtr)
@@ -17,41 +18,40 @@ getFontFiles :: IO [FilePath]
 getFontFiles = do
     let fontDir = "C:\\Windows\\Fonts"
     allFiles <- listDirectory fontDir
+    print allFiles
     -- 筛选出字体文件（例如：.ttf, .otf）
-    filterM isFontFile (map (fontDir </>) allFiles)
+    -- filterM isFontFile (map (fontDir </>) allFiles)
+    return $ map (fontDir </>) allFiles
 
-isFontFile :: FilePath -> IO Bool
-isFontFile path = do
-    let ext = takeExtension path
-    if ext == ".ttf" || ext == ".otf"
-        then return True
-        else do
-            printf "This filename is %s, not so good!\n" path
-            return False
+-- isFontFile :: FilePath -> IO Bool
+-- isFontFile path = do
+--     let ext = takeExtension path
+--     if ext == ".ttf" || ext == ".otf"
+--         then return True
+--         else do
+--             printf "This filename is %s, not so good!\n" path
+--             return False
 
 queryFont :: IO ()
 queryFont = do
     let printEveryLine :: FilePath -> IO ()
         printEveryLine file = do
             print file
-            num <- numFaces file
-            case num of
-                Just n -> printf "faces count %d\n" n
-                _ -> putStrLn "Failed to open font."
+            numFaces file >>= printf "faces count %d\n"
     _ <- Raw.init
     mapM_ printEveryLine =<< getFontFiles
     Raw.quit
 
-numFaces :: FilePath -> IO (Maybe Int)
+numFaces :: FilePath -> IO Int
 numFaces path = do
     font <- withCString path (`Raw.openFont` 12)
     if font == nullPtr
         then do
             putStrLn "Raw.openFont failed"
-            return Nothing
+            return 0
         else do
             num <- fontFaces font
             closeFont font
-            return $ Just $ fromIntegral num
+            return $ fromIntegral num
             
 
