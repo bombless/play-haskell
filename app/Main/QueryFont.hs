419812f0dffa0bf8d28eabb6c5b136edb527910b
York Xiang <bombless@126.com>
2024-12-12 17:24:41 +0800

    Check font faces

--- app/Main/QueryFont.hs
+++ app/Main/QueryFont.hs
@@ -12,14 +12,15 @@ import Foreign.C.String (withCString)
 import Foreign.Ptr (nullPtr)
 
 import SDL.Raw.Font (fontFaces, closeFont)
-import qualified SDL.Raw.Font as Raw (openFont, init, quit)
-import Control.Monad (when)
+import qualified SDL.Raw.Font as Raw (openFont, openFontIndex, init, quit, glyphIsProvided)
+import Control.Monad (when, unless)
+import Foreign.C.Types (CLong)
+import GHC.Base (ord)
 
 getFontFiles :: IO [FilePath]
 getFontFiles = do
     let fontDir = "C:\\Windows\\Fonts"
     allFiles <- listDirectory fontDir
-    print allFiles
     -- 筛选出字体文件（例如：.ttf, .otf）
     -- filterM isFontFile (map (fontDir </>) allFiles)
     return $ map (fontDir </>) allFiles
@@ -30,7 +31,15 @@ queryFont = do
     let printEveryLine :: FilePath -> IO ()
         printEveryLine file = do
             num <- numFaces file
-            when (num > 1) $ printf "% 5d %s\n" num file
+            when (num > 1) $ do
+                printf "% 5d %s\n" num file
+                let showEveryFace :: CLong -> IO ()
+                    showEveryFace i = do
+                        font <- withCString file (\p -> Raw.openFontIndex p 12 i)
+                        printsA <- Raw.glyphIsProvided font (fromIntegral $ ord 'A')
+                        closeFont font
+                        unless (printsA == 0) $ printf "% 5d is good\n" (fromIntegral i :: Int)
+                mapM_ showEveryFace [0 .. fromIntegral num - 1]
     _ <- Raw.init
     fontFiles <- getFontFiles  -- 获取字体文件
     mapM_ printEveryLine fontFiles  -- 执行每个文件的操作
