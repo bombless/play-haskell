c4f22d3e24d1eeb3b28178242635dacc5c4e8079
York Xiang <bombless@126.com>
2024-12-27 00:27:22 +0800

    Gui test

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,11 @@ module Main where
 import Main.Status (runStatus)
 import Main.QueryFont (queryFont)
 import Main.Strings (test)
+import Main.Gui (run)
 
 main :: IO ()
 main = do
     queryFont
     runStatus
     test
+    run
